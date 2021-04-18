c1ce40ace3fc4f109cbe83fe6a96942df7c6f32b
Emmanuel Alabi <emmanuelalabi@Emmanuels-MacBook-Pro.local>
2021-04-18 23:30:01 +0100

    implement product list

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="left">
+        <span className="title">New Products</span>
+        <span className="tagline">Brand new upgrades for your routine</span>
+      </div>
+      <div className="right">
+        <select>
+          <option>Filter By</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Product.css';
+
+const Product = (props) => {
+  const {product, addToCart} = props;
+  return (
+    <div className="product">
+      <img src={product.image_url} alt={product.title} />
+      <span>{product.title}</span>
+      <span>From {props.currency} {product.price}</span>
+      <button onClick={() => addToCart(product)}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default Product;
